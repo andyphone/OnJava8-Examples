3707cccba35db9a97ae79619f9dfd6fb98db70e4
jim.deng <zero4566@sohu.com>
2020-12-01 15:25:21 +0800

    2020年12月1日12:38:06 1

--- collections/LinkedListFeatures.java
+++ collections/LinkedListFeatures.java
@@ -2,7 +2,7 @@
 // (c)2020 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-import typeinfo.pets.*;
+import pets.*;
 import java.util.*;
 
 public class LinkedListFeatures {
@@ -10,29 +10,46 @@ public class LinkedListFeatures {
     LinkedList<Pet> pets =
       new LinkedList<>(Pets.list(5));
     System.err.println(pets);
-    // Identical:
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    // Identical:                   self-note:  getFirst() 和 element() 是相同的，它们都返回列表的头部（第一个元素）而并不删除它，
+    //                              如果 List 为空，则抛出 NoSuchElementException 异常。
     System.err.println(
       "pets.getFirst(): " + pets.getFirst());
     System.err.println(
       "pets.element(): " + pets.element());
-    // Only differs in empty-list behavior:
+    // Only differs in empty-list behavior:               peek() 方法与这两个方法只是稍有差异，它在列表为空时返回 null 。
     System.err.println("pets.peek(): " + pets.peek());
-    // Identical; remove and return the first element:
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    // Identical; remove and return the first element:     如果 List 为空，则抛出 NoSuchElementException 异常。
     System.err.println(
       "pets.remove(): " + pets.remove());
     System.err.println(
       "pets.removeFirst(): " + pets.removeFirst());
-    // Only differs in empty-list behavior:
+    // Only differs in empty-list behavior:                它在列表为空时返回 null 。
     System.err.println("pets.poll(): " + pets.poll());
     System.err.println(pets);
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    /*在列表的开头插入一个元素。*/
     pets.addFirst(new Rat());
     System.err.println("After addFirst(): " + pets);
+
+
+    /*offer() 与 add() 和 addLast() 相同。 它们都在列表的尾部（末尾）添加一个元素。*/
     pets.offer(Pets.get());
     System.err.println("After offer(): " + pets);
     pets.add(Pets.get());
     System.err.println("After add(): " + pets);
     pets.addLast(new Hamster());
     System.err.println("After addLast(): " + pets);
+
+
+    /*删除并返回列表的最后一个元素。*/
     System.err.println(
       "pets.removeLast(): " + pets.removeLast());
   }
--- collections/ListIteration.java
+++ collections/ListIteration.java
@@ -2,7 +2,7 @@
 // (c)2020 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-import typeinfo.pets.*;
+import pets.*;
 import java.util.*;
 
 public class ListIteration {
@@ -18,13 +18,14 @@ public class ListIteration {
     while(it.hasPrevious())
       System.err.print(it.previous().id() + " ");
     System.err.println();
-    System.err.println(pets);
+    System.err.println("TOTAL_1:"+pets);
     it = pets.listIterator(3);
     while(it.hasNext()) {
-      it.next();
-      it.set(Pets.get());
+//      ;
+      System.err.println(it.next() );
+      it.set(Pets.get());//self-note:   使用 set() 方法可以替换它访问过的最近一个元素。
     }
-    System.err.println(pets);
+    System.err.println("TOTAL_2:"+pets);
   }
 }
 /* Output:
@@ -32,6 +33,5 @@ Rat, 1, 0; Manx, 2, 1; Cymric, 3, 2; Mutt, 4, 3; Pug,
 5, 4; Cymric, 6, 5; Pug, 7, 6; Manx, 8, 7;
 7 6 5 4 3 2 1 0
 [Rat, Manx, Cymric, Mutt, Pug, Cymric, Pug, Manx]
-[Rat, Manx, Cymric, Cymric, Rat, EgyptianMau, Hamster,
-EgyptianMau]
+[Rat, Manx, Cymric, Cymric, Rat, EgyptianMau, Hamster, EgyptianMau]
 */
